ffe084201b56560e5ce2d92cfe3547187f8e15ce
Shahzad <shahzadali.infonic@gmail.com>
2021-01-19 17:41:06 +0530

    first Commit

---
+++ src/pages/Home.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Product from "./Product";
+const Home = () => {
+  return (
+
+<section id="aa-product">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="row">
+            <div class="aa-product-area">
+              <div class="aa-product-inner">
+             
+                  
+                  <div class="tab-content">
+                
+                    <div class="tab-pane fade in active" id="men">
+                      <ul class="aa-product-catg">
+                      <Product/>
+                      <Product/>
+                      <Product/>
+                      <Product/>
+                      <Product/>
+                      <Product/>
+                      <Product/>
+                      <Product/>
+                      <Product/>
+                      <Product/>
+                      <Product/>
+                      <Product/>
+                      <Product/>
+                      <Product/>
+                      <Product/>
+                      <Product/>
+                     
+                      
+                       </ul></div></div></div></div></div></div></div>
+                        </div></section>
+                 
+
+);
+};
+
+export default Home;
---
+++ src/pages/Product.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+
+function Product() {
+
+  return (
+    <li>
+   
+    <figure>
+      <a className="aa-product-img">
+      <img src="img/man/polo-shirt-2.png"/></a>
+      <a className="aa-add-card-btn" href="#"><span className="fa fa-shopping-cart"></span>Add To Cart</a>
+        <figcaption>
+        <h4 className="aa-product-title"><a href="#">Polo T-Shirt</a></h4>
+        <span className="aa-product-price">$45.50</span><span className="aa-product-price"><del>$65.50</del></span>
+      </figcaption>
+    </figure>                        
+    <div class="aa-product-hvr-content">
+      <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Add to Wishlist"><span class="fa fa-heart-o"></span></a>
+      <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Compare"><span class="fa fa-exchange"></span></a>
+      <a href="#" data-toggle2="tooltip" data-placement="top" title="" data-toggle="modal" data-target="#quick-view-modal" data-original-title="Quick View"><span class="fa fa-search"></span></a>                          
+    </div>
+ 
+    <span class="aa-badge aa-sale" href="#">SALE!</span>
+ 
+  </li>
+    
+
+  );
+}
+
+export default Product;
